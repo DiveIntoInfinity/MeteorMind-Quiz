b122071b836d3586f10f6ee4b42e667d84f60454
Abhinavpreet Singh Arora <aroraabhinavpreetsingh@gmail.com>
2024-11-05 09:14:34 +0530

    updated sub section

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+
+  
